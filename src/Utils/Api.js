44b5456ca93a5b05424b48db5adeff7eced22070
SergSmilya <sergsmilichenko@gmail.com>
2023-03-16 20:17:46 +0200

    final

--- src/Utils/Api.js
+++ src/Utils/Api.js
@@ -5,21 +5,42 @@ const settingsUrl = {
   URL: 'https://pixabay.com/api/',
   image_type: 'photo',
   orientation: 'horizontal',
-  page: 1,
   per_page: 12,
 };
 
-async function search(values) {
-  const { API_KEY, URL, image_type, orientation, page, per_page } = settingsUrl;
+export default class GalleryApi {
+  constructor() {
+    this.value = '';
+    this.page = 1;
+  }
 
-  try {
-    const response = await axios(
-      `${URL}?key=${API_KEY}&q=${values}&image_type=${image_type}&orientation=${orientation}&page=${page}&per_page=${per_page}`
-    );
-    return response;
-  } catch (error) {
-    return error;
+  async search() {
+    const { API_KEY, URL, image_type, orientation, per_page } = settingsUrl;
+
+    try {
+      const response = await axios(
+        `${URL}?key=${API_KEY}&q=${this.value}&image_type=${image_type}&orientation=${orientation}&page=${this.page}&per_page=${per_page}`
+      );
+      this.incrementPage();
+      return response;
+    } catch (error) {
+      return error;
+    }
+  }
+
+  incrementPage() {
+    this.page += 1;
+  }
+
+  resetPage() {
+    this.page = 1;
+  }
+
+  get readValue() {
+    return this.value;
+  }
+
+  set writeValue(newQuery) {
+    this.value = newQuery;
   }
 }
-
-export { search };
--- src/components/App.jsx
+++ src/components/App.jsx
@@ -1,27 +1,100 @@
-import { search } from '../Utils/Api';
+import React, { Component } from 'react';
 
-export const App = () => {
-  search('cat').then(({ data: { totalHits, hits } }) => {
-    if (totalHits > 0) {
-      // hits це масив
-      console.log(hits);
-    } else {
-      console.log('No hits');
-    }
+import css from './App.module.css';
+import { ToastContainer, toast } from 'react-toastify';
+import 'react-toastify/dist/ReactToastify.css';
+import GalleryApi from '../Utils/Api';
+import Searchbar from './Searchbar/Searchbar';
+import ImageGallery from './ImageGallery/ImageGallery';
+import Button from './Button/Button';
+import Loader from './Loader/Loader';
+
+const requestApi = new GalleryApi();
+
+const notify = () => {
+  toast('Not found', {
+    position: 'top-right',
+    autoClose: 3000,
+    hideProgressBar: false,
+    closeOnClick: true,
+    pauseOnHover: false,
+    draggable: true,
+    progress: undefined,
+    theme: 'light',
   });
-
-  return (
-    <div
-      style={{
-        height: '100%',
-        display: 'flex',
-        justifyContent: 'center',
-        alignItems: 'center',
-        fontSize: 40,
-        color: '#010101',
-      }}
-    >
-      React homework template
-    </div>
-  );
 };
+
+export default class App extends Component {
+  state = {
+    gallerys: [],
+    value: '',
+    visibleSpinerBool: false,
+  };
+
+  handleWriteValueInStateOfSubmit = valueString => {
+    this.setState({ value: valueString });
+    requestApi.resetPage();
+  };
+
+  componentDidUpdate(_, prevState) {
+    if (prevState.value !== this.state.value) {
+      this.setState({ visibleSpinerBool: true });
+      requestApi.writeValue = this.state.value;
+
+      requestApi
+        .search()
+        .then(({ data: { totalHits, hits } }) => {
+          if (totalHits > 0) {
+            // hits це масив
+            // console.log(hits);
+            this.setState({ gallerys: hits });
+          } else {
+            notify();
+          }
+        })
+        .catch(console.log)
+        .finally(this.setState({ visibleSpinerBool: false }));
+    }
+  }
+
+  handleLoadMore = () => {
+    this.setState({ visibleSpinerBool: true });
+    setTimeout(() => {
+      requestApi
+        .search()
+        .then(({ data: { hits } }) => {
+          this.setState(prevState => ({
+            gallerys: [...hits, ...prevState.gallerys],
+          }));
+        })
+        .catch(console.log)
+        .finally(this.setState({ visibleSpinerBool: false }));
+    }, 1000);
+  };
+
+  render() {
+    const { gallerys, visibleSpinerBool } = this.state;
+
+    return (
+      <div className={css.App}>
+        <Searchbar onSubmited={this.handleWriteValueInStateOfSubmit} />
+
+        {visibleSpinerBool ? (
+          <Loader visibleSpinerBool={visibleSpinerBool} />
+        ) : (
+          <ImageGallery gallerys={gallerys} />
+        )}
+
+        {gallerys.length !== 0 && (
+          <Button
+            handleLoadMore={this.handleLoadMore}
+            visibleSpinerBool={visibleSpinerBool}
+          >
+            Load More
+          </Button>
+        )}
+        <ToastContainer />
+      </div>
+    );
+  }
+}
